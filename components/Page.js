98c65c2512aac724378e9837395e7ad759407a98
Nishant Kumar <nishants878@gmail.com>
2021-09-09 22:14:38 +0530

    added product page

--- components/Page.js
+++ components/Page.js
@@ -3,14 +3,14 @@ import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
 
 const GlobalStyles = createGlobalStyle`
-@font-@font-face {
-  font-family: 'radnika_next';
-  src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
-  font-weight: normal;
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
     font-style: normal;
-}
-     html{
-      --red: #ff0000;
+  }
+  html {
+    --red: #ff0000;
     --black: #393939;
     --grey: #3A3A3A;
     --gray: var(--grey);
@@ -20,8 +20,9 @@ const GlobalStyles = createGlobalStyle`
     --maxWidth: 1000px;
     --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
     box-sizing: border-box;
-     }
-     *, *:before, *:after {
+    font-size: 10px;
+  }
+  *, *:before, *:after {
     box-sizing: inherit;
   }
   body {
@@ -33,15 +34,15 @@ const GlobalStyles = createGlobalStyle`
   }
   a {
     text-decoration: none;
-    color: var(---black);
+    color: var(--black);
   }
   a:hover {
     text-decoration: underline;
   }
   button {
-    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
-   `;
+`;
 const InnerStyles = styled.div`
   max-width: var(--maxWidth);
   margin: 0 auto;
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,9 @@
 import PropTypes from 'prop-types';
 import NProgress from 'nprogress';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 // TOdo: swap with our own
 
 import '../components/styles/nprogress.css';
@@ -13,14 +15,30 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+// next js sync method means we can wait on it to do anything like wait for api
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
 MyApp.propTypes = {
   Component: PropTypes.any,
   pageProps: PropTypes.any,
+  apollo: PropTypes.any,
 };
+
+export default withData(MyApp);
