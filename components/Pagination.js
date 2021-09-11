42c8359ff94694670ce25a6b7f3d3cbe12f9559b
Nishant Kumar <nishants878@gmail.com>
2021-09-11 21:27:44 +0530

    added pagination and sign in

---
+++ components/Pagination.js
@@ -0,0 +1,41 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+import PaginationStyles from './styles/PaginationStyles';
+import DisplayError from './ErrorMessage';
+import { perPage } from '../config';
+
+export const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    _allProductsMeta {
+      count
+    }
+  }
+`;
+
+export default function Pagination({ page }) {
+  const { error, data, loading } = useQuery(PAGINATION_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <DisplayError error={error} />;
+  const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count / perPage);
+  return (
+    <PaginationStyles>
+      <Head>Sick Fits - Page {page} of __</Head>
+      <Link href={`/products/${page - 1}`}>
+        <a aria-disabled={page <= 1}>← Prev</a>
+      </Link>
+      <p>
+        Page {page} of {pageCount}
+      </p>
+      <p>{count} Items Total</p>
+
+      <Link href={`/products/${page + 1}`}>
+        <a aria-disabled={page >= pageCount}>Next →</a>
+      </Link>
+    </PaginationStyles>
+  );
+}
